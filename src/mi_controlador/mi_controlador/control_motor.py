5bdb1b7094ba10e6c73803ce3790083f861ac239
Alvaro Perez <aborbujom@gmail.com>
2025-12-03 12:23:19 +0100

    Arreglando posible problema Rx

--- src/mi_controlador/mi_controlador/control_motor.py
+++ src/mi_controlador/mi_controlador/control_motor.py
@@ -13,6 +13,8 @@ class Control_motor:
         self.address=address  
         if conectado:
             print(f"✅ [OK] Roboclaw conectada correctamente. Dirección: {self.address},",flush=True)
+            self.rc._port.timeout = 0.01
+            self.rc._trys = 1
         else:
             print(f"❌ [ERROR] No se pudo abrir el puerto con la Roboclaw en {self.address}",flush=True)
 
