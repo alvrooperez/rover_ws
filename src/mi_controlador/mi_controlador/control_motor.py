4fc7bcef2f64be952b092741ffcb419553017dab
Alvaro Perez <aborbujom@gmail.com>
2025-12-03 13:30:15 +0100

    Pruebas

--- src/mi_controlador/mi_controlador/control_motor.py
+++ src/mi_controlador/mi_controlador/control_motor.py
@@ -121,7 +121,7 @@ def main(args=None):
         #controladora2.motores_bruto(35,35)
         #time.sleep(0.2)
         controladora3.motores_bruto(35,35)
-        #time.sleep(2)
+        time.sleep(2)
         t_fin_comando = time.time()
         print(f"   -> Tiempo enviando comando: {t_fin_comando - t_inicio_comando:.4f} seg", flush=True)
         print("Deteniendo motores",flush=True)
