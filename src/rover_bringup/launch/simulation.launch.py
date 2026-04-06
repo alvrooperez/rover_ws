fe46ecd767996bf30bf5266db04acf2e730156b7
traaph <traaph@gmv.com>
2026-04-06 12:22:39 +0200

    Otro intento imu

--- src/rover_bringup/launch/simulation.launch.py
+++ src/rover_bringup/launch/simulation.launch.py
@@ -35,16 +35,15 @@ def generate_launch_description():
         arguments=[
             '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
             '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
-            # USAMOS LA RUTA COMPLETA AQUÍ:
-            '/model/rover/imu@sensor_msgs/msg/Imu[gz.msgs.IMU', 
+            # Ruta absoluta para Jazzy basada en tu mundo 7x7
+            '/world/7x7/model/rover/link/imu_link/sensor/imu_sensor/imu@sensor_msgs/msg/Imu[gz.msgs.IMU', 
             '/camera/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
             '/camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
             '/odom_gazebo@nav_msgs/msg/Odometry[gz.msgs.Odometry'
         ],
         remappings=[
-            # Y RE-MAPEAMOS PARA QUE ROS LO VEA COMO /imu
-            ('/model/rover/imu', '/imu'),
-        ],
+            ('/world/7x7/model/rover/link/imu_link/sensor/imu_sensor/imu', '/imu'),
+        ]
         parameters=[{'use_sim_time': use_sim_time}],
         output='screen'
     )
